70db9acb7b49202cc710d13e6177ad0cf92d9af1
Doug Sellers <DouglasSSellers@gmail.com>
2025-06-19 16:52:40 -0400

    cleaned up layout with reusable components

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,22 +1,14 @@
+import Branding from '@/components/branding';
 import H1 from '@/components/h1';
+import Stats from '@/components/stats';
 
 export default function Page() {
   return (
     <main>
       <div className="flex items-center justify-between text-white py-8">
-        <section>
-          <H1>
-            Pet <span className="font-semibold">Soft</span>
-          </H1>
-          <p className="text-lg opacity-80">
-            Manage your pet daycare with ease
-          </p>
-        </section>
+        <Branding />
 
-        <section className="text-center">
-          <p className="text-2xl font-bold leading-6">2</p>
-          <p className="opacity-80">current guests</p>
-        </section>
+        <Stats />
       </div>
     </main>
   );
